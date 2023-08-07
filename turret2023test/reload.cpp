00e03147905fb20ca0f78f8f12a99c41949800c6
kayla-tran <97213015+kayla-tran@users.noreply.github.com>
2023-08-07 08:59:59 -0700

    Update reload.cpp

--- turret2023test/reload.cpp
+++ turret2023test/reload.cpp
@@ -1,3 +1,11 @@
+/* GODs Summer 2023 - Will, Eddy, Matthew, Malia, Kayla
+*
+* Description: will shoot any bullet. On button press will shoot the bullet. Wait 1 sec to press again to reload to shoot again.
+* 
+*/
+
+//------------------------------------------------------------------------------------
+
 #include <AccelStepper.h>
 
 #define motorPin1  2
@@ -11,9 +19,11 @@ int degreePerRevolution = 5.625;
 AccelStepper stepper(AccelStepper::HALF4WIRE, motorPin1, motorPin3, motorPin2, motorPin4);
 
 // Degrees of the movement. First, five full rotations (360 degrees each), then back five times.
-int steps[] = {-161,161};
+int steps[] = {-155,155};
 int stepsCount = 2; // Total number of elements in the steps[] array
 
+bool forwardDirection = true;
+bool isMoving = false;
 int stepsIndex = 0;
 
 float degToSteps(float deg) {
@@ -21,14 +31,17 @@ float degToSteps(float deg) {
 }
 
 void reloadSetUp() {
-  stepper.setMaxSpeed(2000.0);
-  stepper.setAcceleration(1500.0);
-  stepper.setSpeed(2000);
+  stepper.setMaxSpeed(4000.0); //3000
+  stepper.setAcceleration(3000.0); //2000
+  stepper.setSpeed(3000); //initial speed 2000
   pinMode(13,INPUT);
 }
 
+int check = 0;
+
 void reloadStepper() {
-  if(digitalRead(13)==HIGH){
+  /*
+  if(digitalRead(13) == HIGH){
     if (!stepper.isRunning() && stepper.distanceToGo() == 0) {
       stepper.moveTo(degToSteps(steps[stepsIndex]));
       stepsIndex++;
@@ -36,7 +49,91 @@ void reloadStepper() {
         stepsIndex = 0;
       }
     }
-  Serial.print("WOKRIN \n");
   }
-stepper.run();
+  stepper.run();
+  */
+
+  
+
+  if (digitalRead(13) == HIGH) {
+    //check = 0;
+    // If the button is pressed, perform the state transition
+    if (!isMoving) {
+      // If currently in state start1, move to state forward
+      if (forwardDirection && stepsIndex == 0) {
+        forwardDirection = true;
+        stepsIndex = 1;
+        check = 10;
+      }
+      // If currently in state forward, move to state backward
+      else if (forwardDirection && stepsIndex == 1) {
+        forwardDirection = false;
+        stepsIndex = 1;
+        check = 20;
+      }
+      // If currently in state backward, move to state forward
+      else if (!forwardDirection && stepsIndex == 1) {
+        forwardDirection = true;
+        stepsIndex = 0;
+        check = 30;
+      }
+    }
+
+  }
+
+  // Perform the stepper movement if it's running
+  if (isMoving) {
+    stepper.run();
+    // Check if the movement is completed
+    if (!stepper.isRunning()) {
+      isMoving = false;
+    }
+  } else {
+    // Start the motor movement based on the current state
+    if (forwardDirection) {
+      stepper.moveTo(degToSteps(steps[stepsIndex]));
+    } else {
+      stepper.moveTo(degToSteps(-steps[stepsIndex]));
+    }
+    isMoving = true;
+  }
+  
+/*
+  if (digitalRead(13) == HIGH) {
+    // If the button is pressed, perform the state transition
+    if (!isMoving) {
+      // If currently in state start1, move to state forward
+      if (forwardDirection && stepsIndex == 0) {
+        forwardDirection = true;
+        stepsIndex = 1;
+      }
+      // If currently in state forward, move to state backward
+      else if (forwardDirection && stepsIndex == 1) {
+        forwardDirection = false;
+        stepsIndex = 1;
+      }
+      // If currently in state backward, move to state forward
+      else if (!forwardDirection && stepsIndex == 1) {
+        forwardDirection = true;
+        stepsIndex = 0;
+      }
+    }
+  }
+
+  // Perform the stepper movement if it's running
+  if (isMoving) {
+    stepper.run();
+    // Check if the movement is completed
+    if (!stepper.isRunning()) {
+      isMoving = false;
+    }
+  } else {
+    // Start the motor movement based on the current state
+    if (forwardDirection) {
+      stepper.moveTo(degToSteps(steps[stepsIndex]));
+    } else {
+      stepper.moveTo(degToSteps(-steps[stepsIndex]));
+    }
+    isMoving = true;
+  }*/
 }
